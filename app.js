a2923cc8b8bb9ed4668084fc74ff17ab4b43b57a
samruddhidev <samruddhi.jain@somaiya.edu>
2026-04-15 19:34:14 +0530

    Enhance game feedback with computer's choice display
    Added functionality to display computer's choice after each match.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
     let compscore=0;
     let choices=document.querySelectorAll(".bts");
     let message=document.querySelector(".msg");
+    let message1=document.querySelector(".msg1");
 
        const compchoice=()=>{
         let ops=["rock","paper","scissor"];
@@ -23,6 +24,7 @@
         {
             console.log("Its A Tie");
             message.innerText="It's A Tie, Try Again!"
+            message1.innerText=`Computer's Choice: ${choiceofcomp.toUpperCase()}`;
         }
         else
         {
@@ -40,25 +42,27 @@
             {
                 userwin=choiceofcomp==="rock" ? false:true;
             }
-showwinner(userwin);
+showwinner(userwin,choiceofcomp);
         }
         }; 
 
-        const showwinner=(userwin)=>{
+        const showwinner=(userwin,choiceofcomp)=>{
             if(userwin==true)
             {
                 userscore++;
                 console.log("You Win");
                 message.innerText="You Win!!!"
                 usersc.innerText=userscore;
-                compsc.innerText=compscore
+                compsc.innerText=compscore;
+                message1.innerText=`Computer's Choice: ${choiceofcomp.toUpperCase()}`;
             }
             else{
                 compscore++;
                 console.log("You Lose");
                 message.innerText="You Lose:((((";
                 usersc.innerText=userscore;
-                compsc.innerText=compscore
+                compsc.innerText=compscore;
+                message1.innerText=`Computer's Choice: ${choiceofcomp.toUpperCase()}`;
                 
             }
         }
